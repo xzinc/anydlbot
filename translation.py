801eeae4e559b07e69d03fc064e072513c22f0e5
Learner <64130518+xzinc@users.noreply.github.com>
2022-03-07 22:30:36 +0530

    Add files via upload

---
+++ translation.py
@@ -0,0 +1,5 @@
+
+class Translation(object):
+    START_TEXT = "Please send me any video link. Supported Sites: https://rg3.github.io/youtube-dl/supportedsites.html"
+    ABS_TEXT = "please subscribe @MalayalamTrollVoice #ForTheGreaterGood "
+    UPGRADE_TEXT = "No upgrades!"
